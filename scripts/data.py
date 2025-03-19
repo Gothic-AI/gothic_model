0dd6b32c1356cd8b65b94e634029d72375c9e45e
PrasanthGubbala <g.prasanth1000@gmail.com>
2025-03-19 12:52:04 -0700

    data collator

---
+++ scripts/data.py
@@ -0,0 +1,17 @@
+from transformers import TextDataset, DataCollatorForLanguageModeling
+
+
+def load_dataset(file_path, tokenizer, block_size = 128):
+    dataset = TextDataset(
+        tokenizer = tokenizer,
+        file_path = file_path,
+        block_size = block_size,
+    )
+    return dataset
+
+def load_data_collator(tokenizer, mlm = False):
+    data_collator = DataCollatorForLanguageModeling(
+        tokenizer=tokenizer, 
+        mlm=mlm,
+    )
+    return data_collator
