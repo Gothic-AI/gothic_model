59198e15f7f986d40e4b01b240abb8f7ddaaa0fd
PrasanthGubbala <g.prasanth1000@gmail.com>
2025-03-26 14:14:30 -0700

    generate text

--- inference/generate_text.py
+++ inference/generate_text.py
@@ -1,6 +1,11 @@
 from utils import generate_text
 
 model1_path = "./models/plain_text_model/"
-sequence1 = "[Q] What is the novel name?"
+model2_path = "./models/q&a_text_model/"
+sequence1 = "Q: Why does Elara ask Jareth about the sky?"
 max_len = 50
-generate_text(model1_path, sequence1, max_len) 
+
+# generated_text = generate_text(model1_path, sequence1, max_len)
+generated_text = generate_text(model2_path, sequence1, max_len)
+print(generated_text)  # This can be printed or used further
+
